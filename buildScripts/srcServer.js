79db93cce1e55d8cf5ae72cb93ab97e4e0585ef0
WymanChen <wyman.58@gmail.com>
2018-10-04 10:53:36 +1000

    staging close project

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,13 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+app.get('/users', function(req,res){
+    res.json([{"id": 1,"firstName":"Bob","lastName":"Smith"},
+              {"id": 2,"firstName":"Tammy","lastName":"Chen"},
+              {"id": 3,"firstName":"Tina","lastName":"Lee"}
+            ]);
+});
+
 app.listen(port, function(err){
     if(err){
         console.log(err); //eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import './index.css';
+import {getUsers} from './api/userApi';
 import numeral from 'numeral';
+
 
 const courseValue = numeral(1000).format('$0,0.00');
 //debugger;
 console.log(`I would pay ${courseValue}`);   //eslint-disable-line no-console
+
+getUsers().then(result=>{
+    let usersBody = "";
+    result.forEach(user=>{
+        usersBody+= `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        </tr>`
+    });
+    global.document.getElementById('users').innerHTML = usersBody;
+});
